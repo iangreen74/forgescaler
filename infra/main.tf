560405796e168773f57c8af4aefb695b5a7318a8
Ian Green <mikahiangreen@gmail.com>
2025-05-18 14:01:06 -0700

    fix(terraform): update backend to use forgescaler-terraform-state bucket

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "forgemind-terraform-state"
+    bucket = "forgescaler-terraform-state"
     key    = "global/terraform.tfstate"
     region = "us-west-2"
   }
@@ -20,40 +20,40 @@ provider "aws" {
 }
 
 module "network" {
-  source              = "./network"
-  name                = "forgemind-vpc"
-  cidr_block          = var.cidr_block
-  availability_zones  = var.availability_zones
-  private_subnets     = var.private_subnets
-  public_subnets      = var.public_subnets
+  source             = "./network"
+  name               = "forgemind-vpc"
+  cidr_block         = var.cidr_block
+  availability_zones = var.availability_zones
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
 }
 
 module "iam" {
-  source     = "./iam"
-  role_name  = var.role_name
+  source    = "./iam"
+  role_name = var.role_name
 }
 
 module "s3" {
-  source       = "./s3"
-  bucket_name  = var.bucket_name
+  source      = "./s3"
+  bucket_name = var.bucket_name
 }
 
 module "acm" {
-  source       = "./acm"
-  domain_name  = var.domain_name
+  source      = "./acm"
+  domain_name = var.domain_name
 }
 
 module "dns" {
-  source          = "./dns"
-  zone_id         = var.zone_id
-  domain_name     = var.domain_name
-  alias_name      = module.eks.cluster_endpoint
-  alias_zone_id   = var.alias_zone_id
+  source        = "./dns"
+  zone_id       = var.zone_id
+  domain_name   = var.domain_name
+  alias_name    = module.eks.cluster_endpoint
+  alias_zone_id = var.alias_zone_id
 }
 
 module "eks" {
-  source          = "./eks"
-  cluster_name    = var.cluster_name
-  subnet_ids      = module.network.private_subnets
-  vpc_id          = module.network.vpc_id
+  source       = "./eks"
+  cluster_name = var.cluster_name
+  subnet_ids   = module.network.private_subnets
+  vpc_id       = module.network.vpc_id
 }
