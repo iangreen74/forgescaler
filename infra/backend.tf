f19b3816658c7477cad467e8cad7b9197de7d0b5
Ian Green <mikahiangreen@gmail.com>
2025-05-18 13:12:22 -0700

    Removed backend.tf to fix Terraform init conflict with CLI-based backend config

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "forgemind-terraform-state"
-    key            = "global/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "forgemind-terraform-locks"
-    encrypt        = true
-  }
-}
