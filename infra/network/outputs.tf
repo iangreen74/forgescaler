cac3fecc6de638089c147483f5c78c42d8f486a7
Ian Green <mikahiangreen@gmail.com>
2025-05-18 19:12:13 -0700

    feat(network): add outputs for vpc_id and subnet IDs for new VPC deployment

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -12,3 +12,23 @@ output "private_subnets" {
   description = "Private subnet IDs"
   value       = module.vpc.private_subnets
 }
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block for the created VPC"
+  value       = aws_vpc.main.cidr_block
+}
