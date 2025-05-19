442956b59c099a4b3b270a48fc7e6bfe4f4e1930
Ian Green <mikahiangreen@gmail.com>
2025-05-18 19:56:13 -0700

    fix(network): clean up variables.tf for VPC and subnet configuration

--- infra/network/variables.tf
+++ infra/network/variables.tf
@@ -1,34 +1,19 @@
-variable "name" {
-  description = "Name of the VPC"
-  type        = string
-  default     = "forgemind-vpc"
-}
-
-variable "cidr_block" {
+variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
-variable "availability_zones" {
-  description = "List of availability zones"
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
-}
-
-variable "private_subnets" {
-  description = "Private subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "public_subnets" {
-  description = "Public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  type        = list(string)
   description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to create subnets in"
+  type        = list(string)
 }
