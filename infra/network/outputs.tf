44c99a8413e865deef287b089514d613952f436d
Ian Green <mikahiangreen@gmail.com>
2025-05-18 20:08:55 -0700

    fix(terraform): remove variable declarations from outputs.tf to resolve conflict

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -1,34 +1,15 @@
-variable "vpc_cidr_block" {
-  description = "CIDR block for the default/original VPC"
-  type        = string
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-variable "public_subnet_cidrs" {
-  description = "Public subnet CIDRs for the original VPC"
-  type        = list(string)
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
-variable "private_subnet_cidrs" {
-  description = "Private subnet CIDRs for the original VPC"
-  type        = list(string)
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
 
-variable "vpc_cidr_block_v2" {
-  description = "CIDR block for the second (new) VPC"
-  type        = string
-}
-
-variable "public_subnet_cidrs_v2" {
-  description = "List of public subnet CIDRs for new VPC"
-  type        = list(string)
-}
-
-variable "private_subnet_cidrs_v2" {
-  description = "List of private subnet CIDRs for new VPC"
-  type        = list(string)
-}
-
-variable "availability_zones" {
-  description = "List of availability zones to deploy resources in"
-  type        = list(string)
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
 }
