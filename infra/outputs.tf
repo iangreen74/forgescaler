8b487afe7d4774ed1b8d54cc0f5f8d4541bf3319
Ian Green <mikahiangreen@gmail.com>
2025-05-20 18:01:22 -0700

    feat(outputs): expose network_v2 outputs for downstream module use

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,14 +2,22 @@ output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
 output "bucket_name" {
   value = module.s3.memory_logs_bucket
 }
 
 output "cert_arn" {
   value = module.acm.cert_arn
+}
+
+output "vpc_id" {
+  value = module.network_v2.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.network_v2.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.network_v2.private_subnet_ids
 }
