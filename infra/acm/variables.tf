1ba3e2c1fa8e26aa814d7a8badce2241bd1cb691
Ian Green <mikahiangreen@gmail.com>
2025-06-03 18:52:06 -0700

    Fix: declare missing variables and remove invalid module inputs

--- infra/acm/variables.tf
+++ infra/acm/variables.tf
@@ -2,3 +2,9 @@ variable "domain_name" {
   description = "The domain name to create a TLS certificate for"
   type        = string
 }
+
+# modules/acm/variables.tf
+variable "zone_id" {
+  description = "The Route53 hosted zone ID"
+  type        = string
+}
